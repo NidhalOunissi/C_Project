4a5a6fa4db8350760efef0d53454d09284bda4dc
ImenJ16 <115788718+ImenJ16@users.noreply.github.com>
2022-11-17 19:07:12 +0100

    Add files via upload

---
+++ BV.c
@@ -0,0 +1,81 @@
+#include "BV.h"
+#include<stdio.h>
+int ajouter(BureauDeVote BV , char filename [])
+{
+    FILE * f=fopen(filename, "a");
+    if(f!=NULL)
+    {
+        fprintf(f,"%d %d %d %s %s %d\n",BV.id,BV.capElecteurs,BV.capObservateurs,BV.salle,BV.adresse, BV.idAgent);
+        fclose(f);
+        return 1;
+    }
+    else return 0;
+}
+
+int modifier(int id, BureauDeVote nouv, char * filename)
+{
+BureauDeVote BV;
+    FILE * f=fopen(filename, "r");
+    FILE * f2 =fopen("aux.txt", "w");
+    if(f==NULL || f2==NULL)
+return 0;
+else
+    {
+while(fscanf(f,"%d %d %d %s %s %d",&BV.id,BV.capElecteurs,&BV.capObservateurs,&BV.salle, &BV.adresse, &BV.idAgent)!=EOF)
+{
+if(BV.id!=id)
+        fprintf(f2,"%d %d %d %s %s %d\n",BV.id,BV.capElecteurs,BV.capObservateurs,BV.salle,BV.adresse, BV.idAgent);
+else
+
+  fprintf(f2,"%d %d %d %s %s %d\n",nouv.id,nouv.capElecteurs,nouv.capObservateurs,nouv.salle, nouv.adresse, nouv.idAgent);
+
+}
+        fclose(f);
+        fclose(f2);
+remove(filename);
+rename("aux.txt", filename);
+        return 1;
+    }
+  
+}
+
+int supprimer(int id, char * filename)
+{
+BureauDeVote BV;
+    FILE * f=fopen(filename, "r");
+    FILE * f2 =fopen("aux.txt", "w");
+    if(f==NULL || f2==NULL)
+return 0;
+else
+    {
+while(fscanf(f,"%d %d %d %s %s %d",&BV.id,BV.capElecteurs,&BV.capObservateurs,&BV.salle, &BV.adresse, &BV.idAgent)!=EOF)
+{
+if(BV.id!=id)
+        fprintf(f2,"%d %d %d %s %s %d\n",BV.id,BV.capElecteurs,BV.capObservateurs,BV.salle,BV.adresse, BV.idAgent);
+
+}
+        fclose(f);
+        fclose(f2);
+remove(filename);
+rename("aux.txt", filename);
+        return 1;
+    }
+}
+
+BureauDeVote chercher(int id, char * filename)
+{
+BureauDeVote BV; int tr=0;
+    FILE * f=fopen(filename, "r");
+ if(f!=NULL )
+    {
+while(fscanf(f,"%d %d %d %s %s %d",&BV.id,BV.capElecteurs,&BV.capObservateurs,&BV.salle, &BV.adresse, &BV.idAgent)!=EOF && tr==0)
+{if(id==BV.id)
+tr=1;
+}
+}
+if(tr==0)
+BV.id=-1;
+return BV;
+
+}
+
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "BV.h"
+#include<stdio.h>
 int main()
 {
+BureauDeVote BV;
+BureauDeVote BV1={1, 2000,10,"35E0","Rue Taieb Mhiri, Rohia Silyana",1} ,BV2={2, 1500,7,"35E1","Rue Al Kods, Rohia Silyana",2};
+ajouter(BV1,"BureauDeVote.txt");
+
+BV= chercher(2,"BureauDeVote.txt");
+if(BV1.id==1)
+printf("inexistant");
+else
+printf("%d %d %d %s %s %d\n",BV.id,BV.capElecteurs,BV.capObservateurs,BV.salle,BV.adresse, BV.idAgent);
 
 }
+
